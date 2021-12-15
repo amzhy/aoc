6cb6489f7d9f9e35676854c03a9ce279b1d34125
amzhy <76540550+amzhy@users.noreply.github.com>
2021-12-16 01:57:29 +0800

    Add files via upload

---
+++ 2b.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int strcmp (const char* str1, const char* str2);
+int main() { 
+    char s[10]; char b[10]="forward", c[10]="up";
+    int a=0, x=0, y=0, d=0;
+   while(scanf("%s", s)==1){
+       scanf("%d", &a);
+        if(strcmp(s,b)==0){
+            x+=a; int e=y*a; d+=e;
+            //printf("%d\n", y);
+        } else if (strcmp(s,c)==0){
+            y-=a;
+        } else{
+            y+=a;
+        }
+   }
+    printf("%d\n",x*d);
+    return 0;
+}
