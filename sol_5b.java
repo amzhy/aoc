69afb2888c5d6b03f418d03dc3a193834e8b9878
amzhy <76540550+amzhy@users.noreply.github.com>
2021-12-17 10:31:32 +0800

    add day 5b

---
+++ sol_5b.java
@@ -0,0 +1,43 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+class sol_5b {
+    public static void main(String[] args) {    
+    Scanner sc = new Scanner(System.in);
+    int[][] val = new int[10000][10000];
+    int ct=0;
+    int[] arr = new int[4];
+    HashSet<String> set =new HashSet<>();
+    while(ct < 500) {
+        String[] s = sc.nextLine().split(",| -> ");
+        for(int i=0; i<4; i++) {
+            arr[i] =Integer.parseInt(s[i]);
+        }
+        int x= Math.min(arr[0], arr[2]); int xd=Math.abs(arr[2]-arr[0]);
+        int y= Math.max(arr[1], arr[3]); int yd=Math.abs(arr[3]-arr[1]);
+
+        if (xd==0 || yd ==0) {
+            if(xd==0){
+                for(int i=Math.min(arr[1], arr[3]);i<=y;i++) {
+                    val[i][x]++;
+                    if(val[i][x] ==2) { set.add("" + i + "" + x); }
+                }
+            }else {
+                for(int i=x; i <= x+xd; i++) {
+                    val[y][i]++;
+                    if(val[y][i]==2) { set.add("" + y + "" + i); }
+                }
+            }
+        } else {
+            int m = (arr[3]-arr[1]) / (arr[2]-arr[0]);  int yc = arr[3]-(arr[2]*m);
+            for(int i=x; i<= x+xd; i++) {
+                int j=yc+(i*m);
+                val[j][i]++;
+                if(val[j][i] ==2) {set.add("" + j + "" + i); }  
+            }
+        }
+        ct++;
+    }
+    System.out.println(set.size());
+}
+}
