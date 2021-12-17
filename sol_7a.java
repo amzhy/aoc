3038864ea227a64f8f065ff177e1b93fc7012b55
amzhy <76540550+amzhy@users.noreply.github.com>
2021-12-17 12:54:14 +0800

    add day 7a

---
+++ sol_7a.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+class sol_7a {
+    public static void main(String[] args) {    
+    Scanner sc = new Scanner(System.in);
+    String[] s = sc.nextLine().split(",");
+
+    int[] val = new int[s.length];
+    int sum=0;
+    HashSet<Integer> set = new HashSet<>();
+    for(int i=0; i < s.length; i++) {
+        val[i] = Integer.parseInt(s[i]);
+        sum += val[i]; set.add(val[i]);
+    }
+    Arrays.sort(val);
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i<val.length;i++)
+        if(map.get(val[i])==null) map.put(val[i], i);
+
+    int min= Integer.MAX_VALUE;
+    for(Integer i: set) {
+        int smaller=0; int k=map.get(i); int cur=0;
+        for(int j=0; j<k;j++) {
+            smaller += (i - val[j]); cur+= val[j];
+        }
+        smaller += (sum-cur) - (i*(val.length-k));
+        min = smaller;
+    }
+    System.out.println(min);
+}
+}
