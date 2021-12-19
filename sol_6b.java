669f981d7ccfd39db6ae48404f092579abdbbf24
amzhy <76540550+amzhy@users.noreply.github.com>
2021-12-19 15:25:26 +0800

    .

--- sol_6b.java
+++ sol_6b.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class sol_6a {
+public class sol_6b {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s[] =  sc.nextLine().split(",");
